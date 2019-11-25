b5bd816c80cfcca64f8b63874b2eb1181405ed5b
shpark1217 <dnjswbf@gmail.com>
2019-11-25 16:57:40 +0900

    master

--- src/depth3/EquipmentWithData.js
+++ src/depth3/EquipmentWithData.js
@@ -10,26 +10,23 @@ class EquipmentWithData extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        selectedKey: -1,
-        selectInfo:[]
+        selectedKey: -1
     }   
     this.handleClick = this.handleClick.bind(this);
 }
 
 handleClick = (key) => {
     this.setState({
-        selectedKey: this.state.selectedKey == key ? -1 : key,
-        selectInfo: this.state.selectInfo.concat({isSelected: true, key: this.selectedKey})
+        selectedKey: this.state.selectedKey == key ? -1 : key
     });
     console.log(this.state.selectedKey);
 }
 
     render() {
-        alert(JSON.stringify(this.state.selectInfo));
       const { equipments, datas } = this.props;
       const list = equipments.map(
         (equipments, i) => (<Equipment2 key={equipments.id} eid={equipments.eid} equipments={equipments} datas={datas} key={i} onClick={() => this.handleClick(i)} 
-        isSelectedKey={this.state.selectInfo.isSelected}/>)
+        isSelectedKey={this.state.selectedKey != -1}/>)
       );
       return(
           <div>
